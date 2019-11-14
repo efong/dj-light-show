421c9c3658ffa6d3d4aaa9a37ead85768cf0ae9e
Anthony <Frenchy2199@yahoo.com>
2019-11-14 11:30:26 -0700

    Added spinning light pattern

--- LightShow.ino
+++ LightShow.ino
@@ -1,3 +1,4 @@
+
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
 #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
@@ -9,7 +10,7 @@
 
 const int speedPotentio = 0;
 const int potentio1 = 1;
-const int potentio2 = 2;
+const int brightPotentio = 2;
 const int changeButton = 2;
 const int circleButton = 3;
 int loopCounter = 0;
@@ -91,15 +92,18 @@ void loop() {
 			snail();
 			break;
 		case 2:
-			smileyFace();
+			spin();
 			break;
 		case 3:
-			expand(0);
+			smileyFace();
 			break;
 		case 4:
-			collapse(0);
+			expand(0);
 			break;
 		case 5:
+			collapse(0);
+			break;
+		case 6:
 			beatPulse();
 			break;
 		}
@@ -133,6 +137,38 @@ void loop() {
   //  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
   //delay(25);
 }
+
+
+void spin()
+{
+	int line1[] = {92, 84, 72, 56, 32, 0};
+	int line2[] = {92, 91, 70, 53, 28};
+	int line3[] = {92, 90, 81, 68, 50, 24};
+	int line4[] = {92, 89, 66, 47, 20};
+	int line5[] = {92, 88, 78, 64, 44, 16};
+	int line6[] = {92, 87, 62, 41, 12};
+	int line7[] = {92, 86, 75, 60, 38, 8};
+	int line8[] = {92, 85, 58, 35, 4};
+	int allLines[] = { line1, line2, line3, line4, line5, line6, line7, line8 };
+	int j = 4;
+	for(int i = 0; i < sizeof(allLines)/ sizeof(*allLines); ++i){
+		strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
+    if(i % 2 == 0){
+      lightUpSpecifics(allLines[i], strip.Color(0, 255, map(analogRead(potentio1), 0, 1023, 0, 255)), 6);
+      lightUpSpecifics(allLines[j], strip.Color(255, 0, map(analogRead(potentio1), 0, 1023, 0, 255)), 6);
+    }
+    else{
+      lightUpSpecifics(allLines[i], strip.Color(0, 255, map(analogRead(potentio1), 0, 1023, 0, 255)), 5);
+      lightUpSpecifics(allLines[j], strip.Color(255, 0, map(analogRead(potentio1), 0, 1023, 0, 255)), 5);
+    }
+    ++j;
+    if(j > 7)
+      j = 0;
+		delay(map(analogRead(speedPotentio), 0, 1023, 15, 400));
+		strip.clear();
+	}
+}
+
 
 /* Immitates light falling from top to bottom, like matrix where there is a wall of numbers (time controlled by potentiometer)
    Would be cool to find a way to not spaghetti code this one
@@ -183,6 +219,7 @@ void matrix(int option)
   //	strip.show();
   //	delay(35);
   strip.setPixelColor(IndexVertical8[v8], strip.Color(153, map(analogRead(potentio1), 0, 1023, 0, 255), 120));
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   strip.show();
   delay(map(analogRead(speedPotentio), 0, 1023, 15, 400));
   strip.clear();
@@ -221,6 +258,7 @@ void matrix(int option)
   //	strip.show();
   //	delay(35);
   strip.setPixelColor(IndexVertical8[v8], strip.Color(153, map(analogRead(potentio1), 0, 1023, 0, 255), 160));
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   strip.show();
   delay(map(analogRead(speedPotentio), 0, 1023, 15, 400));
   strip.clear();
@@ -259,6 +297,7 @@ void matrix(int option)
   //	strip.show();
   //	delay(35);
   strip.setPixelColor(IndexVertical8[v8], strip.Color(153, map(analogRead(potentio1), 0, 1023, 0, 255), 240));
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   strip.show();
   delay(map(analogRead(speedPotentio), 0, 1023, 15, 400));
   strip.clear();
@@ -297,6 +336,7 @@ void matrix(int option)
   //	strip.show();
   //	delay(35);
   strip.setPixelColor(IndexVertical8[v8], strip.Color(153, map(analogRead(potentio1), 0, 1023, 0, 255), 80));
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   strip.show();
   delay(map(analogRead(speedPotentio), 0, 1023, 15, 400));
   strip.clear();
@@ -347,6 +387,7 @@ void matrix(int option)
     lightUpARing(4, strip.Color(150, map(analogRead(potentio1), 0, 1023, 0, 255), 150));
     lightUpARing(6, strip.Color(0, map(analogRead(potentio1), 0, 1023, 0, 255), 255));
   }
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   strip.show();
   delay(map(analogRead(speedPotentio), 0, 1023, 15, 400));
   strip.clear();
@@ -365,9 +406,13 @@ void snail()
   int sizeIndex = 10;	//0-10 is size 11 but we index starting at 0
   int q1 = 0, q2 = 1, q3 = 2, q4 = 3;
   for (int i = 0; i < 5; ++i) {	//5 for 5 iterations total, should make a total loop
+	strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
     lightUpASection(ringStartIndexVertical[q1] + 1, ringStartIndexHorizontal[q1], strip.Color(153, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
+	strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
     lightUpASection(ringStartIndexHorizontal[q2] + 1, ringStartIndexVertical[sizeIndex - q2], strip.Color(153, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
+	strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
     lightUpASection(ringStartIndexVertical[sizeIndex - q3] + 1, ringStartIndexHorizontal[sizeIndex - q3], strip.Color(153, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
+	strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
     lightUpASection(ringStartIndexHorizontal[sizeIndex - q4] + 1, ringStartIndexVertical[q4 + 1], strip.Color(153, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
 
     //    lightUpASection(ringStartIndexVertical[q3] + 1, ringStartIndexHorizontal[q3], strip.Color(153, map(analogRead(potentio1), 0, 1023, 0, 255), map(analogRead(brightPotentio), 0, 1023, 0, 255)));
@@ -407,6 +452,7 @@ void smileyFace()
       strip.clear();
     strip.show();
   }
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   strip.show();
   delay(map(analogRead(speedPotentio), 0, 1023, 0, 300));
   strip.clear();
@@ -423,6 +469,7 @@ void smileyFace()
       strip.clear();
     strip.show();
   }
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   strip.show();
   delay(map(analogRead(speedPotentio), 0, 1023, 0, 300));
   strip.clear();
@@ -439,6 +486,7 @@ void smileyFace()
       strip.clear();
     strip.show();
   }
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   strip.show();
   delay(map(analogRead(speedPotentio), 0, 1023, 0, 300));
   strip.clear();
@@ -455,6 +503,7 @@ void smileyFace()
       strip.clear();
     strip.show();
   }
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   strip.show();
   delay(map(analogRead(speedPotentio), 0, 1023, 0, 300));
   strip.clear();
@@ -472,21 +521,27 @@ void expand(int option) {
 	else{
 		delayAmount = 50;
 	}
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(6, strip.Color(153, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(5, strip.Color(12, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(4, strip.Color(61, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(3, strip.Color(0, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(2, strip.Color(65, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(1, strip.Color(255, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
@@ -504,28 +559,46 @@ void collapse(int option)
 	else{
 		delayAmount = 50;
 	}
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(1, strip.Color(255, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(2, strip.Color(255, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(3, strip.Color(255, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(4, strip.Color(255, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(5, strip.Color(12, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
+  strip.setBrightness(map(analogRead(brightPotentio), 0, 1023, 0, 255));
   lightUpARing(6, strip.Color(153, map(analogRead(potentio1), 0, 1023, 0, 255), 0));
   delay(delayAmount);
   strip.clear();
 }
 
+/* Function that receives an array of leds to light up, different than lightUpASection because that one lights up from start to end (range)
+*  While this function lights up specific leds that are provided to us through an array
+*/
+void lightUpSpecifics(int ledArray[], uint32_t color, int arrSize)
+{
+	for(int i = 0; i < arrSize; ++i){
+		strip.setPixelColor(ledArray[i], color);
+	}
+	strip.show();
+}
+
+
 /* Function that lights up specific leds given the start and end of what needs to be lit up, start is included, end is excluded
-   There is no delay when lighting up a section, it just shows the result, not the progress
+* There is no delay when lighting up a section, it just shows the result, not the progress
 */
 void lightUpASection(int start, int end, uint32_t color)
 {
@@ -583,11 +656,11 @@ void beatPulse()
   for (int i = 0; i < strip.numPixels(); i++) { // For each pixel in strip...
     strip.setPixelColor(i, strip.Color(124, map(analogRead(potentio1), 0, 1023, 0, 255), 0));         //  Set pixel's color (in RAM)
   }
-  strip.setBrightness(200);
+  strip.setBrightness(255);
   strip.show();                          //  Update strip to match
   Serial.println("Boom");
   delay(map(analogRead(speedPotentio), 0, 1023, 20, 400));
-  strip.setBrightness(5);
+  strip.setBrightness(20);
   strip.show();
   delay(map(analogRead(speedPotentio), 0, 1023, 20, 400));
   strip.clear();
@@ -683,7 +756,7 @@ void theaterChaseRainbow(int wait) {
 
 void toggle() {
   state++;
-  if(state >= 6){
+  if(state >= 7){
     state = 0;
   }
 }
